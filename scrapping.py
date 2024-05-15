2db3f654a3d773865ef5b42d38e9b1e00f0e52d0
serii <seriiburduja@gmail.com>
2024-05-15 12:48:28 +0300

    feat: scrapping all pages

--- scrapping.py
+++ scrapping.py
@@ -25,38 +25,67 @@ def saveToJson(filename, data):
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 def handleCategoryPage(all_categories_dict):
+    iteration_count = int(len(all_categories_dict)) - 1
+    print(f"Iteration count: {iteration_count}")
     count = 0
     for category_title, category_link in all_categories_dict.items():
-        # print(f'count: {count}')
-        if count == 0:
-            req = requests.get(category_link, headers=headers)
-            src = req.text
-            with open(f'html/{count}_{category_title}.html', 'w') as file:
-                file.write(src)
-            with open(f'html/{count}_{category_title}.html') as file:
-                src = file.read()
-                soup = BeautifulSoup(src, 'lxml')
-                all_th = soup.find(class_='mzr-tc-group-table').find('thead').find_all('th')
-                product = all_th[0].text
-                calories = all_th[1].text
-                proteins = all_th[2].text
-                fats = all_th[3].text
-                carbohydrates = all_th[4].text
-                if not os.path.isdir('csv'):
-                    os.mkdir('csv')
-                with open(f'csv/{count}_{category_title}.csv', 'w') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(
-                        (
-                            product,
-                            calories,
-                            proteins,
-                            fats,
-                            carbohydrates
-                        )
-                    )
+        print(f'iteration {count} of {iteration_count}')
+        req = requests.get(category_link, headers=headers)
+        src = req.text
+        with open(f'html/{count}_{category_title}.html', 'w') as file:
+            file.write(src)
+        with open(f'html/{count}_{category_title}.html') as file:
+            src = file.read()
+            soup = BeautifulSoup(src, 'lxml')
 
-            count += 1
+            alert_block = soup.find(class_='uk-alert-danger')
+            if alert_block is not None:
+                continue
+
+            all_th = soup.find(class_='mzr-tc-group-table').find('thead').find_all('th')
+            product = all_th[0].text
+            calories = all_th[1].text
+            proteins = all_th[2].text
+            fats = all_th[3].text
+            carbohydrates = all_th[4].text
+            if not os.path.isdir('csv'):
+                os.mkdir('csv')
+            with open(f'csv/{count}_{category_title}.csv', 'w', encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (
+                        product,
+                        calories,
+                        proteins,
+                        fats,
+                        carbohydrates
+                    )
+                )
+        products_tr = soup.find(class_='mzr-tc-group-table').find('tbody').find_all('tr')
+        for item in products_tr:
+            products_tds = item.find_all('td')
+            title = products_tds[0].find('a').text
+            calories = products_tds[1].text
+            proteins = products_tds[2].text
+            fats = products_tds[3].text
+            carbohydrates = products_tds[4].text
+            with open(f'csv/{count}_{category_title}.csv', 'a', encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (
+                        title,
+                        calories,
+                        proteins,
+                        fats,
+                        carbohydrates
+                    )
+                )
+
+        count += 1
+        iteration_count -= 1
+        if iteration_count == 0:
+            print('Scrapping is done')
+            break
 
 def scrapFirstPage():
     src = open('html/index.html').read()
