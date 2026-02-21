72bf9188372c9d176da0d8104a19fb55144d35ae
serii <seriiburduja@gmail.com>
2026-02-21 08:44:32 +0200

    feat: main menu

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from classes.Menu import Menu
+
+
 def main():
-    print("Hello, World!")
+    menu_columns = ["Index", "Option"]
+    menu_rows = [
+        ["0", "View all records"],
+        ["1", "Add a new record"],
+        ["2", "Update an existing record"],
+        ["3", "Delete a record"],
+        ["4", "Exit"],
+    ]
+
+    Menu.display("Main Menu", menu_columns, menu_rows)
 
 
 if __name__ == "__main__":
